532342517ee98f4447fac3f7839c0d089eeb5163
fariha025 <itfariha5@example.com>
2026-05-08 06:22:21 +0300

    Add Railway deployment config and MySQL support

--- AlbaOnboarding/Program.cs
+++ AlbaOnboarding/Program.cs
@@ -11,7 +11,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+{
+    if (builder.Environment.IsProduction())
+        options.UseMySql(connectionString,
+            ServerVersion.AutoDetect(connectionString));
+    else
+        options.UseSqlServer(connectionString);
+});
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Identity
